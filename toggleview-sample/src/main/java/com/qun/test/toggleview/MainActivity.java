15cedd158f521e917853c5d93772fbf935586943
weiqun <331855608@qq.com>
2018-03-29 22:23:35 +0800

    fix bug

--- toggleview-sample/src/main/java/com/qun/test/toggleview/MainActivity.java
+++ toggleview-sample/src/main/java/com/qun/test/toggleview/MainActivity.java
@@ -2,8 +2,6 @@ package com.qun.test.toggleview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ListView;
 
 import com.qun.lib.toggleview.ToggleView;
 
@@ -15,35 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                findViewById(R.id.view1).scrollTo(-100, -100);
-            }
-        });
         mToggleView = findViewById(R.id.toggle_view);
         mToggleView.setChecked(true);
-        findViewById(R.id.btn_test).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(mToggleView.isChecked()) {
-                    mToggleView.setChecked(false);
-                }  else {
-                    mToggleView.setChecked(true);
-                }
-            }
-        });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-//        mToggleView.setChecked(true);
     }
 }
